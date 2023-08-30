c40d1e319165a1522aa6d69e4df006a12eefbd97
HrishikShaji <hrishikminishaji@gmail.com>
2023-08-30 21:53:55 +0530

    upload done

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -15,6 +16,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -64,7 +66,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6 ">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
