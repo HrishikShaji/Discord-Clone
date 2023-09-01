ce172cf25a52e429cd81c32bdaaba36b29059dde
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-01 23:08:28 +0530

    kick and role change

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
 import { InviteModal } from "../modals/create-invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
+import { MembersModal } from "../modals/members-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +20,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
