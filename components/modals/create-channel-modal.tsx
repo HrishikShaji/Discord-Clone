1342779cd764e61aec99afe6fd085375362ccba7
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-03 23:04:23 +0530

    server sidebar channels done

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -50,19 +50,28 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModalStore();
+  const { isOpen, onClose, type, data } = useModalStore();
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
+  const { channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isloading = form.formState.isSubmitting;
 
